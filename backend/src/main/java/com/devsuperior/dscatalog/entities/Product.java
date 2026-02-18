9666efa10a9f5678c4f0a561a18c24fda20a546a
Edson Ney de Farias <edson.ney@gmail.com>
2026-02-18 19:15:25 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -19,7 +19,6 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 	
 	@Id
